cbe28209834fbffdb399fb68bfa6ce9cfcf115f4
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2024-03-07 12:35:20 +0300

    appearance child elements with delay

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -57,7 +57,13 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <ul id="new-challenge-images">
+        <motion.ul 
+          id="new-challenge-images"
+          // appearance child elements with delay
+          variants={{
+            visible: { transition: { staggerChildren: 0.05 } }
+          }}>
+          {/* animate wrapper we can animate child elements using variants */}
           {images.map((image) => (
             <motion.li
               variants={{
@@ -73,7 +79,7 @@ export default function NewChallenge({ onDone }) {
               <img {...image} />
             </motion.li>
           ))}
-        </ul>
+        </motion.ul>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
